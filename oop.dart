9ba2aa4ff267658c8f7740f4edb02b63a340f564
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-04-03 08:24:07 +0300

    ft:function - create a data file to read data from

--- oop.dart
+++ oop.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 // Define an interface with a method
 abstract class Car {
   void startEngine();
@@ -15,5 +16,51 @@ class Truck extends Lorry {
   @override
   void startEngine() {
     print('Roooaaaam!!!');
+  }
+}
+
+ // Creates a class that represents a Car instance initialized with data from a file
+class CarFromTxtFile implements Car {
+  String engineStatus;
+
+  CarFromTxtFile({required this.engineStatus});
+
+  @override
+  void startEngine() {
+    print('Engine status: $engineStatus');
+  }
+
+  // Method to read data from file and initialize the instance
+  static Future<CarFromTxtFile> readFromFile(String filePath) async {
+    try {
+      // Read data from file
+      var file = File(filePath);
+      var engineStatus = await file.readAsString();
+
+      // Initialize CarFromFile instance with data from file
+      return CarFromTxtFile(engineStatus: engineStatus);
+    } catch (e) {
+      // Handle any errors
+      print('Error reading file: $e');
+      // Return a default instance if reading fails
+      return CarFromTxtFile(engineStatus: 'Unknown');
+    }
+  }
+}
+
+void main() async {
+  // Read data from file and initialize CarFromFile instance
+  var carFromFile = await CarFromTxtFile.readFromFile('vehicle_data.txt');
+
+  // Call startEngine method to demonstrate the functionality
+  carFromFile.startEngine(); // Output vehicle_data.txt data read from file
+
+  // Create an instance of a class that overrides an inherited method
+  var lorry = Lorry();
+  lorry.startEngine(); 
+
+  // Method that demonstrates the use of a loop --for loop
+  for (var i = 0; i < 5; i++) {
+    print('Audi RS4- : $i');
   }
 }
